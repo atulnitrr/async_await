b43b4f5c27af2c9870b1262ba2ad84f724fce1e7
atulnitrr <atulkumar458@gmail.com>
2019-06-25 08:43:14 +0530

    Promise

--- promises.js
+++ promises.js
@@ -19,7 +19,7 @@ const createPost = post => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       posts.push(post);
-      const error = true;
+      const error = false;
       if (!error) {
         resolve();
       } else {
@@ -29,8 +29,22 @@ const createPost = post => {
   });
 };
 
-createPost({ title: "Post four", body: "This is post four" })
-  .then(getPost)
-  .catch(error => {
-    console.log(error);
-  });
+// createPost({ title: "Post four", body: "This is post four" })
+//   .then(getPost)
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// Promise.all
+const promise1 = Promise.resolve("Hello world ");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, "Good bye");
+});
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
+  res.json()
+);
+
+Promise.all([promise1, promise2, promise3, promise4]).then(value =>
+  console.log(value)
+);
